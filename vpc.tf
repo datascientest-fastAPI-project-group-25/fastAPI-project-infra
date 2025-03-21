7fe19b0ad9a301bab3a1e6054c948ffe5e35b392
jaron <jaronschulz@gmail.com>
2025-03-21 16:36:43 +0100

    Configure OIDC and add Makefile

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "MyVPC" }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2a"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
