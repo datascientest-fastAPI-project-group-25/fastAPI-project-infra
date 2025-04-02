8911deaa1363656c4c0ab80be79de401e0585395
jaron <jaronschulz@gmail.com>
2025-04-02 17:05:04 +0200

    Fix missing aws_account_id parameter in security module

--- bootstrap/environments/aws/main.tf
+++ bootstrap/environments/aws/main.tf
@@ -17,7 +17,7 @@ locals {
   aws_region = coalesce(var.aws_region, "eu-west-2")
   s3_bucket_name = coalesce(var.state_bucket_name, "fastapi-project-terraform-state-${var.aws_account_id}")
   logs_bucket_name = coalesce(var.logs_bucket_name, "fastapi-project-terraform-logs-${var.aws_account_id}")
-  
+
   # Common tags
   common_tags = {
     Environment = var.environment
@@ -61,6 +61,7 @@ module "security" {
   state_bucket_id   = module.state.state_bucket_id
   lambda_zip_path   = var.lambda_zip_path
   aws_region        = local.aws_region
+  aws_account_id    = var.aws_account_id
 
   resource_arns = [
     module.state.state_bucket_arn,
